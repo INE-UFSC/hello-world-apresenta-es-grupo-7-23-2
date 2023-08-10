f3ced0ddf7744c6140688e405940fd7542c8f2d5
MarcosVini16 <128490552+MarcosVini16@users.noreply.github.com>
2023-08-10 15:34:11 -0300

    mudei só uma coisinha no texto

--- membro2.py
+++ membro2.py
@@ -1,2 +1,2 @@
 def hello():
-	return " Oi, sou o Marcos, tenho 18 anos. Hobbies: cuidar do meu cachorro, jogar FFX e LOR (lol de carta). Experiências prévias: só o primeiro semestre da faculdade. O que espero: (dor e sofrimento) aprender mais sobre orientação a objetos."
+	return " Oi, sou o Marcos, tenho 18 anos. Hobbies: cuidar do meu cachorro, jogar FFX e LOR (lol de carta). Experiências prévias: só o primeiro semestre da faculdade. O que espero: aprender mais sobre orientação a objetos."
